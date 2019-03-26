6ab6256c7f12bf67032267c497642d2cfe5e2dfd
Mahesh Manik Sonavane <maheshmanik.sonavane@dnata.com>
2019-03-27 00:16:00 +0400

    update

--- python/Basic Data Types/challenge-07 percentage/percentage.py
+++ python/Basic Data Types/challenge-07 percentage/percentage.py
@@ -1,14 +1,12 @@
 # refereces:
 # https://www.sololearn.com/Discuss/1347790/meaning-of-adding-an-asterisk-before-a-variable-in-python 
 
-def getPercentage(student_name,student_marks):
-    if student_name in student_marks:
-        total = 0
-        subjectCount = len(student_marks[student_name])
-        for mark in student_marks[student_name]:
-            total += mark
-    return total/subjectCount
-
+def getPercentage(query_name,student_marks):
+    percentage=None
+    if query_name in student_marks:
+        query_scores = student_marks[query_name]
+        percentage=sum(query_scores)/len(query_scores)
+    return percentage
 
 if __name__ == '__main__':
     n = int(input())
@@ -18,4 +16,7 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
-    print("{0:.2f}".format(getPercentage(query_name,student_marks)))
+    if getPercentage(query_name,student_marks) != None:
+        print("{0:.2f}".format(getPercentage(query_name,student_marks)))
+    else:
+        print("specify correct student name for query!!")
